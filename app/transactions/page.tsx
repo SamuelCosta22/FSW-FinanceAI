57013f0e18dc35bd34ec2543f34427d833ed45a8
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2024-11-16 17:46:36 -0300

    feat: ensure get dashboard function is protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -23,7 +23,7 @@ const TransactionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6 overflow-hidden">
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
