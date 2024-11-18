613809e00dd575138c4e2058d18ce232d535237d
SamuelCosta22 <samuelgcostaintitucional@gmail.com>
2024-11-18 16:31:52 -0300

    chore: modifying subscription card size

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -11,19 +11,21 @@ const SubscriptionPage = async () => {
     redirect("/login");
   }
 
-  return(
+  return (
     <>
       <Navbar />
-      <div className="p-6 space-y-6">
-        <h1 className="font-bold text-2xl">Assinatura</h1>
+      <div className="space-y-6 p-6">
+        <h1 className="text-2xl font-bold">Assinatura</h1>
         <div className="flex gap-6">
-          <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
-              <h2 className="text-center text-2xl font-semibold">Plano Básico</h2>
-              <div className="flex items-center gap-3 justify-center">
-                <span className="text-4xl">R$</span>
-                <span className=" font-semibold text-6xl">0</span>
-                <span className="text-muted-foreground text-2xl">/mês</span>
+          <Card className="w-[400px]">
+            <CardHeader className="border-b border-solid py-6">
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Básico
+              </h2>
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-3xl">R$</span>
+                <span className="text-5xl font-semibold">0</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
               </div>
             </CardHeader>
             <CardContent className="space-y-6 py-8">
@@ -38,13 +40,15 @@ const SubscriptionPage = async () => {
             </CardContent>
           </Card>
 
-          <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
-              <h2 className="text-center text-2xl font-semibold">Plano Premium</h2>
-              <div className="flex items-center gap-3 justify-center">
-                <span className="text-4xl">R$</span>
-                <span className=" font-semibold text-6xl">19</span>
-                <span className="text-muted-foreground text-2xl">/mês</span>
+          <Card className="w-[400px]">
+            <CardHeader className="border-b border-solid py-6">
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Premium
+              </h2>
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-3xl">R$</span>
+                <span className="text-5xl font-semibold">19</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
               </div>
             </CardHeader>
             <CardContent className="space-y-6 py-8">
