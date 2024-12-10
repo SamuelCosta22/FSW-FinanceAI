39ce638d43f78b8d72178400548d4fed0d26d3f4
SamuelCosta22 <samuelgcostaintitucional@gmail.com>
2024-12-10 08:38:28 -0300

    refactor: ensure Decimal warning does not happen

--- app/_data/_get-dashboard/index.ts
+++ app/_data/_get-dashboard/index.ts
@@ -3,82 +3,102 @@ import { TransactionType } from "@prisma/client";
 import { TotalExpensePerCategory, TransactionPercentagePerType } from "./types";
 import { auth } from "@clerk/nextjs/server";
 
-export const getDashboard = async(month: string) => {
-    const {userId} = await auth();
-    if(!userId){
-        throw new Error("Unauthorized");
-    }
+export const getDashboard = async (month: string) => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
 
-    const where = {
-        userId,
-        date: {
-            gte: new Date(`2024-${month}-01`),
-            lt: new Date(`2024-${month}-31`)
-        }
-    }
+  const where = {
+    userId,
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lt: new Date(`2024-${month}-31`),
+    },
+  };
 
-    const depositsTotal = Number((await db.transaction.aggregate({
-        where: {...where, type: "DEPOSIT"},
-        _sum: {amout: true}
-    }))._sum?.amout);
+  const depositsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "DEPOSIT" },
+        _sum: { amout: true },
+      })
+    )._sum?.amout,
+  );
 
-    const investmentsTotal = Number((await db.transaction.aggregate({
-        where: {...where, type: "INVESTMENT"},
-        _sum: {amout: true}
-    }))._sum?.amout);
+  const investmentsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "INVESTMENT" },
+        _sum: { amout: true },
+      })
+    )._sum?.amout,
+  );
 
-    const expensesTotal = Number((await db.transaction.aggregate({
-        where: {...where, type: "EXPENSE"},
-        _sum: {amout: true}
-    }))._sum?.amout);
+  const expensesTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "EXPENSE" },
+        _sum: { amout: true },
+      })
+    )._sum?.amout,
+  );
 
-    const balance = depositsTotal - investmentsTotal - expensesTotal;
+  const balance = depositsTotal - investmentsTotal - expensesTotal;
 
-    const transactionsTotal = Number((
-        await db.transaction.aggregate({
+  const transactionsTotal = Number(
+    (
+      await db.transaction.aggregate({
         where,
         _sum: { amout: true },
-        })
-    )._sum.amout);
+      })
+    )._sum.amout,
+  );
 
-    const typesPercentage: TransactionPercentagePerType = {
-        [TransactionType.DEPOSIT]: Math.round(
-          (Number(depositsTotal || 0) / Number(transactionsTotal)) * 100,
-        ),
-        [TransactionType.EXPENSE]: Math.round(
-          (Number(expensesTotal || 0) / Number(transactionsTotal)) * 100,
-        ),
-        [TransactionType.INVESTMENT]: Math.round(
-          (Number(investmentsTotal || 0) / Number(transactionsTotal)) * 100,
-        ),
-    };
+  const typesPercentage: TransactionPercentagePerType = {
+    [TransactionType.DEPOSIT]: Math.round(
+      (Number(depositsTotal || 0) / Number(transactionsTotal)) * 100,
+    ),
+    [TransactionType.EXPENSE]: Math.round(
+      (Number(expensesTotal || 0) / Number(transactionsTotal)) * 100,
+    ),
+    [TransactionType.INVESTMENT]: Math.round(
+      (Number(investmentsTotal || 0) / Number(transactionsTotal)) * 100,
+    ),
+  };
 
-    const totalExpensePerCategory: TotalExpensePerCategory[] = (
-        await db.transaction.groupBy({
-            by: ["category"],
-            where: {
-                ...where,
-                type: TransactionType.EXPENSE,
-            },
-            _sum: {
-                amout: true,
-            },
-        })).map((category) => ({
-            category: category.category,
-            totalAmount: Number(category._sum.amout),
-            percentageOfTotal: Math.round(
-                (Number(category._sum.amout) / Number(expensesTotal)) * 100,
-            ),
-        })
-    );
-
-    const lastTransactions = await db.transaction.findMany({
-        where,
-        orderBy: {date: "desc"},
-        take: 10,
+  const totalExpensePerCategory: TotalExpensePerCategory[] = (
+    await db.transaction.groupBy({
+      by: ["category"],
+      where: {
+        ...where,
+        type: TransactionType.EXPENSE,
+      },
+      _sum: {
+        amout: true,
+      },
     })
+  ).map((category) => ({
+    category: category.category,
+    totalAmount: Number(category._sum.amout),
+    percentageOfTotal: Math.round(
+      (Number(category._sum.amout) / Number(expensesTotal)) * 100,
+    ),
+  }));
 
-    return {
-        balance, depositsTotal, investmentsTotal, expensesTotal, typesPercentage, totalExpensePerCategory, lastTransactions
-    }
-}
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: { date: "desc" },
+    take: 10,
+  });
+
+  return {
+    balance,
+    depositsTotal,
+    investmentsTotal,
+    expensesTotal,
+    typesPercentage,
+    totalExpensePerCategory,
+    lastTransactions: JSON.parse(JSON.stringify(lastTransactions)),
+  };
+};
